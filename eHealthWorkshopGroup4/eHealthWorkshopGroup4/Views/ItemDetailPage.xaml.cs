e0d300e662a08ceb395817b1d0c492a521ea6dad
roeyZemmel <roey.zemel@gmail.com>
2018-11-28 11:40:28 +0200

    add dummy project

---
+++ eHealthWorkshopGroup4/eHealthWorkshopGroup4/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using eHealthWorkshopGroup4.Models;
+using eHealthWorkshopGroup4.ViewModels;
+
+namespace eHealthWorkshopGroup4.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ItemDetailPage : ContentPage
+    {
+        ItemDetailViewModel viewModel;
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = viewModel;
+        }
+
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+
+            var item = new Item
+            {
+                Text = "Item 1",
+                Description = "This is an item description."
+            };
+
+            viewModel = new ItemDetailViewModel(item);
+            BindingContext = viewModel;
+        }
+    }
+}
