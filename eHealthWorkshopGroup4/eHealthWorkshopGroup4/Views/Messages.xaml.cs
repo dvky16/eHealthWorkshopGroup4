c38bad80a59c08e4e5db79b75b59b6f107191f0a
Bar Rousso <Bar.Rousso@outlook.com>
2018-12-15 23:49:28 +0200

    change in messages page

--- eHealthWorkshopGroup4/eHealthWorkshopGroup4/Views/Messages.xaml.cs
+++ eHealthWorkshopGroup4/eHealthWorkshopGroup4/Views/Messages.xaml.cs
@@ -6,15 +6,32 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
+  
 namespace eHealthWorkshopGroup4.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Messages : ContentPage
 	{
-		public Messages ()
-		{
-			InitializeComponent ();
-		}
-	}
+        static List<Services.Message> MessageList;
+
+		public Messages()
+        {
+            InitializeComponent();
+            MessageList = new List<Services.Message>();
+            CoachMessages.ItemsSource = MessageList;
+            MessageList.Add(new Services.Message("Message1", "15/12/2018", "This is mesage #1"));
+            MessageList.Add(new Services.Message("Message2", "15/12/2018", "This is mesage #2"));
+            MessageList.Add(new Services.Message("Message3", "15/12/2018", "This is mesage #3"));
+            MessageList.Add(new Services.Message("Message4", "15/12/2018", "This is mesage #4"));
+            MessageList.Add(new Services.Message("Message5", "15/12/2018", "This is mesage #5"));
+            
+        }
+
+      /*  private async void ButtonListener(Object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ContentMessagePage());
+        }
+        */
+
+    }
 }
