f132ec62548d3551beea9c26d6d7600418ea8619
dvky16 <kydaveloper@gmail.com>
2018-12-15 16:01:57 +0200

    Started designing the Info page

--- eHealthWorkshopGroup4/eHealthWorkshopGroup4/Views/Info.xaml.cs
+++ eHealthWorkshopGroup4/eHealthWorkshopGroup4/Views/Info.xaml.cs
@@ -15,6 +15,28 @@ namespace eHealthWorkshopGroup4.Views
 		public Info ()
 		{
 			InitializeComponent ();
+
+            Label header = new Label{
+                Text = "Archive",
+                FontSize = 50,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Button excerciseButton = new Button{
+                Text = "push ups",
+                BorderWidth = 1,
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayuotOptions.CenterAndExpand
+            };
+            excerciseButton.Clicked += OnButtonClicked;
+
+            Content = new StackLayout() {
+                Children = {
+                    header, excerciseButton
+                }
+            };
 		}
 	}
 }
